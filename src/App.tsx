f2878c7036328db4d1a96f8b371248ffc5664a68
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-09-25 18:13:21 +0200

    parse a midi file

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import logo from './logo.svg';
+import { Midi } from '@tonejs/midi';
 import './App.css';
 
 function App() {
+  const ref = useRef<HTMLTextAreaElement>(null);
+
+  function loadImage(event: React.ChangeEvent<HTMLInputElement>) {
+    if(event && event.target && event.target.files && ref.current) {
+      console.log("jdklfjdklfjkdfjl")
+      const file: File = event.target.files[0];
+      parseFile(file);
+    }
+  }
+
+  function parseFile(file : File) {
+    //read the file
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      if(!ref.current || !e || !e.target) {
+        return;
+      }
+      const result  = e.target.result as ArrayBuffer;
+      console.log(result);
+      const midi = new Midi(result);
+      ref.current.value = JSON.stringify(midi, undefined, 2);
+    };
+    reader.readAsArrayBuffer(file);
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +45,8 @@ function App() {
         >
           Learn React
         </a>
+        <input type="file" onChange={loadImage} />
+        <textarea ref={ref}/>
       </header>
     </div>
   );

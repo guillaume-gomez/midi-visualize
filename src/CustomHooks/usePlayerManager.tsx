ae0bd539b0a5599b421d5577ee1b1df7325542e0
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-12-08 14:45:41 +0100

    store if the players can play

--- src/CustomHooks/usePlayerManager.tsx
+++ src/CustomHooks/usePlayerManager.tsx
@@ -6,6 +6,7 @@ const initialState = {}
 
 function usePlayerManager(state = initialState) {
   const [play, setPlay] = useState<boolean>(false);
+  const [couldPlay, setCouldPlay] = useState<boolean>(false);
   const [midi, setMidi] = useState<Midi>();
   const [filepath, setFilepath] = useState<string>("");
 
@@ -17,13 +18,32 @@ function usePlayerManager(state = initialState) {
     setPlay(false);
   }
 
+  function couldPlayFunction(midi: Midi, filepath: string) {
+    setCouldPlay(filepath !== "" && !!midi);
+  }
+
+  function setMidiFunction(value: Midi) {
+    setMidi(value);
+    if(value) {
+      couldPlayFunction(value, filepath);
+    }
+  }
+
+  function setFilepathFunction(value: string) {
+    setFilepath(value);
+    if(midi) {
+      couldPlayFunction(midi, value);
+    }
+  }
+
   return {
     play: playFunction,
     pause,
     midi,
     filepath,
-    setMidi,
-    setFilepath
+    setMidi: setMidiFunction,
+    setFilepath: setFilepathFunction,
+    couldPlay
   }
 }
 
